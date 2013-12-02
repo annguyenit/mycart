7399f7e7610c4ae83f1bd18e963f96cada818be4
annguyen <annguyenphp@gmail.com>
2013-12-03 02:36:19 +0700

    commit all source

---
+++ catalog/language/vietnamese/common/maintenance.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title']     = 'Bảo Trì Hệ Thống';
+
+// Text
+$_['text_maintenance']  = 'Bảo Trì Hệ Thống';
+$_['text_message']      = '<h1 style="text-align:center;">Chúng tôi đang tiến hàng bảo trì hệ thống định kì. <br/>Chúng tôi sẽ hoạt động lại sớm nhất có thể. Mời bạn vui lòng quay lại sau! Trân trọng cám ơn!</h1>';
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mycart/');
+define('HTTP_IMAGE', 'http://localhost/mycart/image/');
+define('HTTP_ADMIN', 'http://localhost/mycart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mycart/');
+define('HTTPS_IMAGE', 'http://localhost/mycart/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/catalog/');
+define('DIR_SYSTEM', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/system/');
+define('DIR_DATABASE', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/system/database/');
+define('DIR_LANGUAGE', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/system/config/');
+define('DIR_IMAGE', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/image/');
+define('DIR_CACHE', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/download/');
+define('DIR_LOGS', 'C:\Program Files (x86)\Zend\Apache2\htdocs\mycart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'mycart');
+define('DB_PREFIX', 'oc_');
+?>
